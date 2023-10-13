50213659a7cc6042b46babd1b18f52e2d31fe00d
omenapuu-admin <omenapuu.web@gmail.com>
2023-10-13 18:16:48 +0300

    Disable horizontal scroll

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -8,6 +8,16 @@
 //
 
 window.addEventListener('DOMContentLoaded', event => {
+    
+    var docWidth = document.documentElement.offsetWidth;
+    [].forEach.call(
+    document.querySelectorAll('*'),
+    function(el) {
+        if (el.offsetWidth > docWidth) {
+        console.log(el);
+        }
+    }
+    );
 
     // Navbar shrink function
     var navbarShrink = function () {
